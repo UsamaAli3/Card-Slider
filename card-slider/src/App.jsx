cd61e38449e23b6eb8735b0b34777995f1268d3e
Usama Ali <imusamaali7@gmail.com>
2024-07-08 18:50:47 +0500

    scroller code

--- card-slider/src/App.jsx
+++ card-slider/src/App.jsx
@@ -1,35 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const cards = [
+    { id: 1, title: "Card 1", content: "This is card 1" },
+    { id: 2, title: "Card 2", content: "This is card 2" },
+    { id: 3, title: "Card 3", content: "This is card 3" },
+    { id: 4, title: "Card 4", content: "This is card 4" },
+    { id: 5, title: "Card 5", content: "This is card 5" },
+  ];
 
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handlePrevClick = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? cards.length - 1 : prevIndex - 1
+    );
+  };
+
+  const handleNextClick = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === cards.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+  console.log(currentIndex);
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="flex justify-center items-center w-6/12 p-auto m-auto gap-7">
+        <div>
+          <div className="bg-blue-500 w-72 h-72 flex flex-col items-center justify-center">
+            <div className="py-auto font-bold text-2xl ">
+              {cards[currentIndex].id} :This is Tital:{" "}
+              {cards[currentIndex].title}
+            </div>
+            <div className="py-auto">
+              This is Paragraph: {cards[currentIndex].content}
+            </div>
+          </div>
+          <div className="bg-blue-500 w-72 h-72 flex flex-col items-center justify-center">
+            <div className="py-auto font-bold text-2xl ">
+              {cards[currentIndex].id} :This is Tital:{" "}
+              {cards[currentIndex].title}
+            </div>
+            <div className="py-auto">
+              This is Paragraph: {cards[currentIndex].content}
+            </div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <div className="bg-blue-500 flex justify-center m-auto p-auto">
+        <button onClick={handlePrevClick} className="mr-10 bg-black">
+          left
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <button onClick={handleNextClick} className=" bg-black">
+          right
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
